67da911ccebcc2823af7d8e9d54e1c9a9593deaf
unknown <redoyhasan4585@gmail.com>
2024-07-15 22:35:52 +0400

    working on login page added from

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,21 +1,41 @@
+// import react phone number input
+import "react-phone-number-input/style.css";
+import PhoneInput from "react-phone-number-input";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 const Login = () => {
+  // state for control number
+  const [phoneNumber, setPhoneNumber] = useState();
+
   return (
     <main>
       <div className="hero bg-base-200 min-h-screen">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">Login now!</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-          </div>
-          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+        <div className="hero-content ">
+          <div className="card bg-base-100 w-full max-w-lg min-w-[500px] shrink-0 shadow-2xl">
+            <h1 className=" text-xl font-bold text-center py-3">Please Login with Number/Email.</h1>
             <form className="card-body">
+        
+              {/* Number  */}
               <div className="form-control">
                 <label className="label">
-                  <span className="label-text">Email</span>
+                  <span className="text-base md:text-lg font-semibold">
+                    Phone Number
+                  </span>
+                </label>
+                <PhoneInput
+                  placeholder="Enter phone number"
+                  value={phoneNumber}
+                  onChange={setPhoneNumber}
+                  className="input input-bordered"
+                />
+              </div>
+              {/* email  */}
+              <div className="form-control">
+                <label className="label">
+                  <span className="text-base md:text-lg font-semibold">
+                    Email
+                  </span>
                 </label>
                 <input
                   type="email"
@@ -26,24 +46,22 @@ const Login = () => {
               </div>
               <div className="form-control">
                 <label className="label">
-                  <span className="label-text">Password</span>
+                  <span className="text-base md:text-lg font-semibold">
+                    PIN
+                  </span>
                 </label>
                 <input
-                  type="password"
-                  placeholder="password"
+                  type="number"
+                  placeholder="PIN"
                   className="input input-bordered"
                   required
                 />
-                <label className="label">
-                  <a href="#" className="label-text-alt link link-hover">
-                    Forgot password?
-                  </a>
-                </label>
               </div>
               <div className="form-control mt-6">
                 <button className="btn btn-primary">Login</button>
               </div>
             </form>
+            <h4 className=" text-base text-center py-3">New in HH Commerze <Link className=" font-bold text-blue-600" to={'/register'}>Register!</Link> </h4>
           </div>
         </div>
       </div>
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,10 +1,81 @@
+// import react phone number input
+import "react-phone-number-input/style.css";
+import PhoneInput from "react-phone-number-input";
+import { useState } from "react";
 
 const Register = () => {
+  // state for control number
+  const [phoneNumber, setPhoneNumber] = useState();
+
   return (
-    <div>
-      Register
-      
-    </div>
+    <main>
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content ">
+          <div className="card bg-base-100 w-full max-w-lg min-w-[500px] shrink-0 shadow-2xl">
+            <form className="card-body">
+              {/* Name */}
+              <div className="form-control">
+                <label className="label">
+                  <span className="text-base md:text-lg font-semibold">
+                    Full Name
+                  </span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="full name"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              {/* Number  */}
+              <div className="form-control">
+                <label className="label">
+                  <span className="text-base md:text-lg font-semibold">
+                    Phone Number
+                  </span>
+                </label>
+                <PhoneInput
+                  placeholder="Enter phone number"
+                  value={phoneNumber}
+                  onChange={setPhoneNumber}
+                  className="input input-bordered"
+                />
+              </div>
+              {/* email  */}
+              <div className="form-control">
+                <label className="label">
+                  <span className="text-base md:text-lg font-semibold">
+                    Email
+                  </span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="text-base md:text-lg font-semibold">
+                    PIN
+                  </span>
+                </label>
+                <input
+                  type="number"
+                  placeholder="PIN"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Login</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 };
 
