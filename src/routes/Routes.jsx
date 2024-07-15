c0a488ea4f94b67f241aa0f2ec2716bcd4f3857b
unknown <redoyhasan4585@gmail.com>
2024-07-15 21:36:41 +0400

    first commit

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "../Layout/Layout";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout></Layout>,
+    children:[
+      
+      {
+        path:'/login',
+        element:<Login></Login>
+      },
+      {
+        path:'/register',
+        element:<Register></Register>
+      },
+    ]
+  },
+]);
+export default router;
